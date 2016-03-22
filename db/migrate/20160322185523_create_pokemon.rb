71554d7b682725e280b9e754920362c1b3f0babe
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-22 16:07:22 -0400

    Add columns to createPokemon migration, re-migrate

--- db/migrate/20160322185523_create_pokemon.rb
+++ db/migrate/20160322185523_create_pokemon.rb
@@ -3,6 +3,16 @@ class CreatePokemon < ActiveRecord::Migration
     create_table :pokemon do |t|
       t.integer :national_poke_num, null: false
       t.string :name, null: false
+      t.string :type_one
+      t.string :type_two
+      t.integer :generation
+      t.integer :total_points
+      t.integer :base_hp
+      t.integer :base_attack
+      t.integer :base_defense
+      t.integer :base_sp_attack
+      t.integer :base_sp_defense
+      t.integer :base_speed
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20160322193040) do
   create_table "pokemon", force: :cascade do |t|
     t.integer  "national_poke_num", null: false
     t.string   "name",              null: false
+    t.string   "type_one"
+    t.string   "type_two"
+    t.integer  "generation"
+    t.integer  "total_points"
+    t.integer  "base_hp"
+    t.integer  "base_attack"
+    t.integer  "base_defense"
+    t.integer  "base_sp_attack"
+    t.integer  "base_sp_defense"
+    t.integer  "base_speed"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
   end
