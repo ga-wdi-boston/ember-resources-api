ceb4dde63440e0f89bca2d31731e418afb3cb19b
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-23 16:00:22 -0400

    Reorder migrations and run rake db:migrate
    Put CreateItems migration before CreatePokemon migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322193040) do
+ActiveRecord::Schema.define(version: 20160322185523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
